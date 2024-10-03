c6fbaac5992b8cf2b23a80320693dce5ee877096
dhev390 <sayuragimhan5@gmail.com>
2024-10-03 19:23:36 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || '', // Enter Your Session ID
+    SESSION_ID: process.env.SESSION_ID || 'wXUHBSCL#pyAnmuJ-JVJ6YInauhwDexqA6yGr9czJXHOil3ZUswE', // Enter Your Session ID
     MONGODB: process.env.MONGODB || 'mongodb+srv://kpd:@cluster0.jlje5.mongodb.net/?retryWrites=true&w=majority',    // Enter Your MongoDB URL
     Owner: process.env.OwnerNumber || '94766494699',    // Enter Your Owner Number
     BotNumber: process.env.BotNumber || '94766494699'    // Enter Your Bot Number
